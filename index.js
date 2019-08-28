fb0a85f84a26a0c8d9be08c1422ec84e2577a684
Piotr Osoba <piotrosoba@outlook.com>
2019-08-28 21:22:22 +0200

    second version

--- index.js
+++ index.js
@@ -1,16 +1,72 @@
-let numbers = []
+const winnerNumbersDiv = document.querySelector('.winner-numbers')
 
-for (let i = 1; i <= 42; i++) {
-  numbers.push(i)
+class Ball {
+  constructor(container, num) {
+    this.num = num
+    this.div = container
+    this.container = {
+      width: container.offsetWidth,
+      height: container.offsetHeight
+    }
+
+    this.ball = null
+
+    this.createBall()
+  }
+  createBall() {
+    this.ball = document.createElement('div')
+
+    this.ball.classList.add('ball')
+    this.ball.style.width = this.width + 'px'
+    this.ball.style.height = this.height + 'px'
+    this.ball.innerText = this.num
+
+    return this
+  }
+
+  appendBall() {
+    this.div.appendChild(this.ball)
+
+    return this
+  }
+
+  move() {
+
+  }
 }
 
-const winning = []
+class Lotto {
+  constructor() {
+    this.container = document.querySelector('.game-box')
+    this.balls = []
+    this.lossed = 0
 
-for (let i = 0; i < 5; i++) {
-  const number = Math.floor(Math.random() * numbers.length)
-  winning.push(numbers[number])
-  numbers = numbers.filter((el, index) => index !== number)
+    for (let i = 1; i <= 42; i++) {
+      const ball = new Ball(this.container, i)
+      ball.createBall().appendBall()
+      this.balls.push(ball)
+    }
+  }
+
+  loss() {
+    if (!this.balls.length || this.lossed >= 5) return
+    const random = Math.floor(Math.random() * this.balls.length)
+    const winnerBall = this.balls[random]
+    winnerNumbersDiv.innerHTML += '<div>' + (winnerBall.num) + '</div>'
+    winnerBall.ball.classList.add('winner-ball')
+    winnerBall.ball.style.backgroundColor = 'red'
+    setTimeout(() => {
+      winnerBall.ball.classList.remove('winner-ball')
+    }, 500)
+
+    this.lossed++
+
+    this.balls = this.balls.filter((ball, index) => random !== index)
+  }
 }
 
-console.log(winning)
+const lotto = new Lotto()
+
+document.querySelector('button').addEventListener('click', () => lotto.loss())
+
 
